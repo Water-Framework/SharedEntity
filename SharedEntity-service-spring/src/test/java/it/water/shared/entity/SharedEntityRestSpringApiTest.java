0076b1a5f3c42e818a56ad472a1566e8a0a8cc60
Aristide <aristide.cittadino@acsoftware.it>
2024-10-25 07:56:57 +0200

    adding more tests and setting up spring api services

--- SharedEntity-service-spring/src/test/java/it/water/shared/entity/SharedEntityRestSpringApiTest.java
+++ SharedEntity-service-spring/src/test/java/it/water/shared/entity/SharedEntityRestSpringApiTest.java
@@ -27,17 +27,28 @@ public class SharedEntityRestSpringApiTest {
     @Autowired
     private Runtime runtime;
 
+    @Autowired
+    private TestEntitySystemApi testEntitySystemApi;
+
+    private TestEntityResource testEntityResource;
+
     @BeforeEach
     void impersonateAdmin() {
         //jwt token service is disabled, we just inject admin user for bypassing permission system
         //just remove this line if you want test with permission system working
         TestRuntimeUtils.impersonateAdmin(componentRegistry);
+        //Adding a specific test resource in order to make the sharing system work
+        testEntityResource = new TestEntityResource();
+        testEntityResource.setEntityVersion(1);
+        testEntityResource.setId(1);
+        testEntityResource.setUserOwner(userIntegrationClient.fetchUserByUserId(runtime.getSecurityContext().getLoggedEntityId()));
+        testEntitySystemApi.save(testEntityResource);
     }
 
     @Karate.Test
     Karate restInterfaceTest() {
         return Karate.run("../SharedEntity-service/src/test/resources/karate")
-                .systemProperty("testEntityResourceId", String.valueOf(1))
+                .systemProperty("testEntityResourceId", String.valueOf(testEntityResource.getId()))
                 .systemProperty("userId", String.valueOf(runtime.getSecurityContext().getLoggedEntityId()));
     }
 
--- SharedEntity-service/README.md
+++ SharedEntity-service/README.md
@@ -1,1 +1,4 @@
 # SharedEntity Service Module
+
+Shared Entity Service is a side-service embedded inside micro-service which rapresents the domain-aggreate.
+It is useful when the user wants to expose sharing service across entities.
