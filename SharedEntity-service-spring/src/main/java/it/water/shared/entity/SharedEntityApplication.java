e71bcc6cebfa9696eab658f80d4b8f17f3640814
Aristide <aristide.cittadino@acsoftware.it>
2025-04-19 18:24:56 +0200

    fix spring scan package

--- SharedEntity-service-spring/src/main/java/it/water/shared/entity/SharedEntityApplication.java
+++ SharedEntity-service-spring/src/main/java/it/water/shared/entity/SharedEntityApplication.java
@@ -27,9 +27,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableWaterFramework
-@EnableJpaRepositories(basePackages = {"it.water.*", "it.water.shared.entity.*"}, repositoryFactoryBeanClass = RepositoryFactory.class)
-@EntityScan({"it.water.*"})
-@ComponentScan({"it.water.*", "it.water.shared.entity.*"})
+@EnableJpaRepositories(basePackages = {"it.water"}, repositoryFactoryBeanClass = RepositoryFactory.class)
+@EntityScan({"it.water"})
+@ComponentScan({"it.water"})
 public class SharedEntityApplication {
     public static void main(String[] args) {
         SpringApplication.run(SharedEntityApplication.class, args);
